3e881b270b614d1363462eb2305c8c30934a7ac8
Abhishek Chaudhary <abhishekchaudhary0220@gmail.com>
2020-11-14 11:01:52 +0530

    signup

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from "@angular/forms";
+import { StoreInfoService }  from '../services/store-info.service';
+import { Router } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-register',
@@ -6,10 +10,47 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  signupForm: FormGroup;
+  constructor(
+    public fb: FormBuilder,
+    private http: HttpClient,
+    private storeInfo: StoreInfoService,
+    private router: Router
+  ) {
+    this.mainForm();
   }
 
+  ngOnInit(){
+    if(this.storeInfo.isSignedIn){
+      this.router.navigateByUrl('');
+      return;
+    }
+  }
+  mainForm() {
+    this.signupForm = this.fb.group({
+      username: ['', [Validators.required]],
+      email: ['', [Validators.required]],
+      password: ['', [Validators.required]],
+      phone_number: ['', [Validators.required]]
+    })
+  }
+  async signUp(){
+    console.log("sign press", this.storeInfo.isSignedIn)
+    if(!this.signupForm.valid) return false;
+    else{
+      this.http.post(this.storeInfo.serverUrl + '/registration',this.signupForm.value).pipe().subscribe((data)=>{
+        if(!data["username"]){
+          console.log("invalid");
+          this.storeInfo.signOut();
+          return;
+        }
+        else{
+          console.log("done");
+          this.router.navigateByUrl('login');
+        }
+      },error =>{
+        console.log(error)
+      })
+    }
+  }
 }
